c76c2e83e57b459fa4e1321e1ed1825b55ef5028
kpijenifer <jenifermech7@gmail.com>
2022-08-12 11:46:29 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,146 @@
+1.
+let a={ name:"jenifer",height:"170cm",weight:"60kg"};
+               console.log("my name is", a.name,a.height,a.weight);
+               console.log(a);
+
+
+
+ 2. 
+ let b={brande:"yammaha",model:"rs200",fueltype:"petrol",color:"white"};
+   console.log("my bike is",b.brande,b.model,b.fueltype,b.color);
+   console.log(b);
+
+   3.
+   let c={brande:"mobile",model:"redmi",ram:"6gb",color:"gold"};
+   console.log("my mobile is",c.brande,c.model,c.ram,c.color)
+   console.log(c);
+    
+   4 
+   let d={brande:"laptop",model:"lenovo",ram:"6gb",color:"black"};
+   console.log("my laptop is",d.brande,d.model,d.ram,d.color);
+   console.log(d);
+
+   5.
+   let e={brande:"fridge",model:"samsung",size:"260L",color:"red"};
+   console.log("my bridge is",e.brande,e.model,e.size,e.color);
+   console.log(e);
+
+   6.
+   let f={company:"bovonto",color:"black" ,litter:"1"};
+   console.log("my bovondo is",f.company,f.color,f.litter);
+   console.log(f);
+
+   7.
+   let g={brande:"T-shirt",size:"l-size",color:"whhite"};
+   console.log("my shirt is ",g.brande,g.size,g.color);
+   console.log(g);
+   8.
+   let h={model:"Tv",brand:"sony",size:"43inc",color:"black"};
+   console.log("my tv is",h.model,h.brande,h.size,h.color);
+   console.log(h)
+
+   9.
+   let i={model:"tyre",brand:"mrf",color:"black"};
+   console.log("my tyre is ",i.model,i.brand,i.size,i.color);
+   console.log(i);
+
+   10.
+   let j={model:"fan",brand:"usha",color:"white"};
+   console.log("my fan is",j.model,j.brand,j.color);
+   console.log(j);
+   11.
+   let k={model:"watch",brand:"sony",color:"black"};
+   console.log("my watch is",k.model,k.brand,k.color);
+   console.log(k);
+
+    12.
+let tractor={brand:"John deere",Model:"5310",color:"Red",price:"1500000"};
+console.log("my tractor is",tractor.brand,tractor.model,tractor.color,tractor.price);
+console.log(tractor);
+
+    13.
+let headset={brand:"Skull-Candy",type:"Bluetooth headset",price:"4500"};
+console.log("my headset is",headset.brand,headset.type,headset.price);
+console.log(headset);
+
+    14.
+let shirt={brand:"Raymond",type:"Party wear",color:"Black",price:"4000"};
+console.log("my shirt is",shirt.brand,shirt.type,shirt.color,shirt.price);
+console.log(shirt);
+
+    15.
+let bag={brand:"Skybags",type:"Water-proof with hifi look",color:"Black & violet",price:"3000"};
+console.log("my bag is",bag.brand,bag.type,bag.color,bag.price);
+console.log(bag);
+    16.
+let pc={brand:"Intel",displaysize:"22 inches display",Processor:"'core i3 11th generation'",price:"25000"}
+console.log("my pc is",pc.brand,pc.displaysize,pc.Processor,pc.price);
+console.log(pc);
+    17.
+let bulb={brand:"philips",volt:"24v",price:"450",type:"plastic-material"};
+console.log("my bulb is",bulb.brand,bulb.volt,bulb.price,bulb.type);
+console.log(bulb);
+    18.
+let note={brand:"Classmate",type:"Unruled longsize",price:"Rs.50"};
+console.log("my note is",note.brand,note.type,note.price);
+console.log(note);
+    19.
+let keyboard={brand:"prolite",keycolor:"Black & White",price:"300",connection:"wired"};
+console.log("my keyboard is",keyboard.brand, keyboard.keycolor,keyboard.price,keyboard.connection);
+console.log(keyboard);
+    20.
+let chocalate={brand:"Dairy Milk",Type:"Badam with chocolate",price:"125"};
+console.log("my chocalate is",chocalate.brand, chocalate.type,chocalate.price);
+console.log(chocalate);
+
+    21.
+let ice={brand:"Amul",Flavour:"Oreo with double Choco",Price:"60"};
+console.log("my ice is",ice.brand,ice.Flavour,ice.price);
+console.log(ice);
+    22. 
+let icecream={brand:"jeni",Flavour:"Raspberry",price:"60"};
+console.log("my icecream is",icecream.brand,ice.flavour,icecream.price);
+console.log(icecream);
+    23.
+let cycle={model:"cycle",brand:"atles",price:"1000",color:"green"};
+console.log("my cycle is ",cycle.model,cycle.brand,cycle.price,cycle.color);
+console.log(cycle);
+
+    24.
+let motor={model:"motor",brand:"texmo",speed:"600rpm",color:"black"};
+console.log("my motor is ",motor.model,motor.brand,motor.speed,motor.color);
+console.log(motor);
+
+    25.
+let  radio={model:"radio",brand:"lave",frequency:"1000",color:"red"};
+console.log("my radio is",radio.model,radio.brand,radio.frequency,radio.color);
+console.log(radio);
+
+
+    26.
+let cigratte={brand:"Gold-Flake",type:"Long-shot",price:"180 per box"};
+console.log("the cigratte is",cigratte.brand,cigratte.type,cigratte.price);
+console.log(cigratte);
+
+
+    27.
+    let Bottle={brand:"Tupperware",Type:"High Plastic",Resistant:"Hot & Cold",price:"450"};
+    console.log("the Bottle is",Bottle.brand,Bottle.type,Bottle.resistant,Bottle.price);
+    console.log(Bottle);
+
+    28.
+let cup={brand:"Aldivo",Material:"Galzed ceramic",color:"BLack and White",price:"800"};
+console.log("the cup is" ,cup.brand,cup.Material,cup.color,cup.price);
+console.log(cup);
+    29.
+let pipe={brand:"Aqua",Size:"All size(mm)",type:"Plastic",Price:"Differ based on their size"};
+console.log("the pipe is",pipe.brand,pipe.size,pipe.type,pipe.price,);
+console.log(pipe);
+
+    30.
+let drinks={brand:"Apple Fizz",Quantity:"1000ml",Flavour:"Apple",Price:"40"};
+console.log("requrie is brand",drinks.brand,drinks.quantity,drinks.Flavour,drinks.Price);
+console.log(drinks);
+   
+   
+              
